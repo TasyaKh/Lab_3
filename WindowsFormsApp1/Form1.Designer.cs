0a68e8ddeaaa4096e516101df164105fef014e5e
tasya132002@gmail.com <tasya132002@gmail.com>
2021-10-07 22:07:04 +0800

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.comboBox6 = new System.Windows.Forms.ComboBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.comboBox7 = new System.Windows.Forms.ComboBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(406, 385);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 47);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(119, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Цвет";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(121, 7);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Насыщенность";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(122, 11);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Яркость";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(122, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(52, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(123, 27);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(50, 22);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(124, 31);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(50, 22);
+            this.textBox3.TabIndex = 6;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Red",
+            "Green",
+            "Blue"});
+            this.comboBox1.Location = new System.Drawing.Point(9, 30);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(94, 24);
+            this.comboBox1.TabIndex = 7;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "degr.",
+            "%",
+            "pt."});
+            this.comboBox2.Location = new System.Drawing.Point(179, 30);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(49, 24);
+            this.comboBox2.TabIndex = 8;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "%",
+            "pt."});
+            this.comboBox3.Location = new System.Drawing.Point(179, 25);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(48, 24);
+            this.comboBox3.TabIndex = 9;
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            "%",
+            "pt."});
+            this.comboBox4.Location = new System.Drawing.Point(180, 29);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(48, 24);
+            this.comboBox4.TabIndex = 10;
+            // 
+            // comboBox5
+            // 
+            this.comboBox5.FormattingEnabled = true;
+            this.comboBox5.Items.AddRange(new object[] {
+            "+",
+            "-"});
+            this.comboBox5.Location = new System.Drawing.Point(54, 69);
+            this.comboBox5.Name = "comboBox5";
+            this.comboBox5.Size = new System.Drawing.Size(49, 24);
+            this.comboBox5.TabIndex = 11;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(122, 69);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(52, 22);
+            this.textBox4.TabIndex = 12;
+            // 
+            // comboBox6
+            // 
+            this.comboBox6.FormattingEnabled = true;
+            this.comboBox6.Items.AddRange(new object[] {
+            "+",
+            "-"});
+            this.comboBox6.Location = new System.Drawing.Point(55, 65);
+            this.comboBox6.Name = "comboBox6";
+            this.comboBox6.Size = new System.Drawing.Size(49, 24);
+            this.comboBox6.TabIndex = 13;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(123, 67);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(52, 22);
+            this.textBox5.TabIndex = 14;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(122, 70);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(52, 22);
+            this.textBox6.TabIndex = 16;
+            // 
+            // comboBox7
+            // 
+            this.comboBox7.FormattingEnabled = true;
+            this.comboBox7.Items.AddRange(new object[] {
+            "+",
+            "-"});
+            this.comboBox7.Location = new System.Drawing.Point(54, 68);
+            this.comboBox7.Name = "comboBox7";
+            this.comboBox7.Size = new System.Drawing.Size(49, 24);
+            this.comboBox7.TabIndex = 15;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.comboBox2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.comboBox5);
+            this.panel1.Controls.Add(this.textBox4);
+            this.panel1.Location = new System.Drawing.Point(25, 38);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(273, 108);
+            this.panel1.TabIndex = 17;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.comboBox3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.textBox2);
+            this.panel2.Controls.Add(this.comboBox6);
+            this.panel2.Controls.Add(this.textBox5);
+            this.panel2.Location = new System.Drawing.Point(25, 152);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(273, 108);
+            this.panel2.TabIndex = 18;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.comboBox4);
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Controls.Add(this.textBox3);
+            this.panel3.Controls.Add(this.textBox6);
+            this.panel3.Controls.Add(this.comboBox7);
+            this.panel3.Location = new System.Drawing.Point(25, 279);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(273, 108);
+            this.panel3.TabIndex = 19;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.panel3);
+            this.panel4.Controls.Add(this.panel2);
+            this.panel4.Controls.Add(this.panel1);
+            this.panel4.Location = new System.Drawing.Point(27, 12);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(325, 420);
+            this.panel4.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(665, 450);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.ComboBox comboBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.ComboBox comboBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.ComboBox comboBox7;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+    }
+}
+
---
+++ WindowsFormsApp1/HSV.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+enum TypeValue
+{
+    DEGREES, PERSENT, PORTION, 
+}
+
+enum TypeColor
+{
+    RED, GREEN, BLUE
+}
+
+namespace WindowsFormsApp1
+{
+    class HSV
+    {
+        private double hue;         //Цветовой тон
+        private double saturation;  //Насыщенность
+        private double brighteness; //Яркость
+
+        private TypeValue typeHue;        //Тип данных для цвета (доступно 3 типа)
+        private TypeValue typeSaturation; //Тип данных для насыщенности (доступно 2 типа)
+        private TypeValue typeBright;     //Тип данных для Яркости (доступно 2 типа)
+
+        private TypeColor CurrentColor;
+        private String messageAboutError;
+        public HSV(short hue, short saturation, short brighteness)
+        {
+            typeHue = TypeValue.DEGREES;
+            typeSaturation = TypeValue.PERSENT;
+            typeBright = TypeValue.PERSENT;
+
+            this.hue = hue;
+            this.saturation = saturation;
+            this.brighteness = brighteness;
+        }
+        public void setHSB(TypeValue typeHue, TypeValue typeSaturation, TypeValue typeBright)
+        {
+            this.typeHue = typeHue;
+            this.typeSaturation = typeSaturation;
+            this.typeBright = typeBright;
+        }
+
+        public void setTypeHue(TypeValue typeHue)
+        {
+            this.typeHue = typeHue;
+        }
+        public void setTypeSaturation(TypeValue typeSaturation)
+        {
+            this.typeSaturation = typeSaturation;
+        }
+        public void setTypeValue(TypeValue typeBright)
+        {
+            this.typeBright = typeBright;
+        }
+
+        private double colorToDegrees(double color)
+        {
+            double colorDegrees = 0;
+
+            switch (typeHue)
+            {
+                case TypeValue.DEGREES:
+                    colorDegrees = color;
+                    break;
+                case TypeValue.PERSENT:
+                    colorDegrees = 360/100 * color;
+                    break;
+                case TypeValue.PORTION:
+                    colorDegrees = 360 * color;
+                    break;
+            }
+            return colorDegrees;
+        }
+
+        private double plusMinusColor(double value, bool plus)
+        {
+            double color;
+
+            if (plus)
+            {
+                color = hue + value;
+            }
+            else
+                color = hue - value;
+
+            return color;
+        }
+        public String checkColor(double value, bool plus)
+        {
+            String messageAboutError = "";
+            double color = plusMinusColor(value, plus);
+  
+            color = colorToDegrees(color);
+
+            if (color > 360)
+                messageAboutError = "Ошибка!Некорректно задано значение цвета: " +
+                    $"{ color}(> 360 град.)";
+            else if (color < 0)
+                messageAboutError = $"Ошибка!Диапазон цвета:{color} < 0";
+            else
+            {
+                switch (CurrentColor)
+                {
+                    case TypeColor.RED:
+                        messageAboutError =  checkDiapasonRed(color);
+                        break;
+                    case TypeColor.GREEN:
+                        messageAboutError = checkDiapasonGreen(color);
+                        break;
+                    case TypeColor.BLUE:
+                        messageAboutError = checkDiapasonBlue(color);
+                        break;
+                }
+            }
+            return messageAboutError;
+        }
+        public String checkDiapasonRed(double color)
+        {
+            String messageAboutError = "";
+
+            if (!(color < 120 || color == 360))
+                messageAboutError = $"Значение цвета({color}) превысило краcный диапазон цвета(0 - 120, 360 град.)";
+
+            return messageAboutError;
+        }
+        public String checkDiapasonGreen(double color)
+        {
+            String messageAboutError = "";
+
+            if (!(color >= 120 && color < 240))
+                messageAboutError = $"Значение цвета({color}) превысило зеленый диапазон цвета(120-240 град.)";
+            return messageAboutError;
+        }
+        public String checkDiapasonBlue(double color)
+        {
+            String messageAboutError = "";
+            if (!(color >= 240 && color < 360))
+                messageAboutError = $"Значение цвета({color}) превысило синий диапазон цвета(240-360 град.)";
+            return messageAboutError;
+        }
+
+        public bool checkCorrDatesField(String field,int numField)
+        {
+            bool isNumber = false;
+            double number;
+
+            try
+            {
+                number = Convert.ToDouble(field);
+                if (number < 0)
+                {
+                    messageAboutError = "Ошибка!Значение не может быть отрицательным.Введите число > 0)+" +
+                        $"Проверьте поле №{numField}";
+                }  
+                else 
+                   isNumber = true;
+            }
+            catch (FormatException)
+            {
+                messageAboutError = $"Ошибка!Введены недопустимые символы: " + field + ".Введите числовой тип данных)"+
+                    $"Проверьте поле №{numField}";
+            
+            }
+            return isNumber;
+        }
+        public String getMessageError()
+        {
+            return messageAboutError;
+        }
+       
+    }
+}
